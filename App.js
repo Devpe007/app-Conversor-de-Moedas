e542c9eb286893b3b1af1926179698aa38f33c05
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-06-17 22:55:14 -0300

    finalized interface app

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 
 import Picker from './src/components/Picker';
 
@@ -9,6 +9,22 @@ export default function App() {
       <View style={styles.areaMoeda}>
         <Text style={styles.titulo}>Selecione sua moeda</Text>
         <Picker/>
+      </View>
+      <View style={styles.areaValor}>
+        <Text style={styles.titulo}>Digite um valor para converter em (R$)</Text>
+        <TextInput
+          placeholder="EX: 150"
+          style={styles.input}
+          keyboardType="numeric"
+        />
+      </View>
+      <TouchableOpacity style={styles.botaoArea}>
+        <Text style={styles.botaoTexto}>Converter</Text>
+      </TouchableOpacity>
+      <View style={styles.areaResultado}>
+        <Text style={styles.valorConvertido}>3 USD</Text>
+        <Text style={[styles.valorConvertido, { fontSize: 18, margin: 10 }]}>Corresponde a</Text>
+        <Text style={styles.valorConvertido}>19,90</Text>
       </View>
     </View>
   );
@@ -34,5 +50,46 @@ const styles = StyleSheet.create({
     color: '#000000',
     paddingTop: 5,
     paddingLeft: 5,
+  },
+  areaValor:{
+    width: '90%',
+    backgroundColor: '#f9f9f9',
+    paddingBottom: 9,
+    paddingTop: 9,
+  },
+  input:{
+    width: '100%',
+    padding: 10,
+    height: 45,
+    fontSize: 20,
+    marginTop: 8,
+    color: '#000000',
+  },
+  botaoArea:{
+    width: '90%',
+    backgroundColor: '#fb4b57',
+    height: 45,
+    borderBottomLeftRadius: 9,
+    borderBottomRightRadius: 9,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  botaoTexto:{
+    fontSize: 18,
+    color: '#ffffff',
+    fontWeight: 'bold',
+  },
+  areaResultado:{
+    width: '90%',
+    backgroundColor: '#ffffff',
+    marginTop: 35,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 25,
+  },
+  valorConvertido:{
+    fontSize: 39,
+    fontWeight: 'bold',
+    color: '#000000',
   },
 });
